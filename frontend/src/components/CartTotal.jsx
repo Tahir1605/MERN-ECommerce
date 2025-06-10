0f04bd8907c027338cc2fdf823e17b8aeeea3ccf
Tahirul Islam <5566tahirul@gmail.com>
2025-06-10 09:52:33 +0530

    new changes

--- frontend/src/components/CartTotal.jsx
+++ frontend/src/components/CartTotal.jsx
@@ -4,6 +4,9 @@ import Title from './Title';
 
 function CartTotal() {
     const {currency,delivery_fee,getCartAmount} = useContext(ShopContext);
+    const subTotal = getCartAmount();
+    const shippingFee = getCartAmount() > 100 ? delivery_fee : 0;
+    const totalAmount = subTotal + shippingFee;
   return (
     <div className='w-full'>
       <div className='text-2xl'>
@@ -12,17 +15,17 @@ function CartTotal() {
       <div className='flex flex-col gap-2 mt-2 text-sm'>
           <div className='flex justify-between'>
               <p>Subtotal</p>
-              <p>{currency}{getCartAmount()}.00</p>
+              <p>{currency}{subTotal}.00</p>
           </div>
           <hr />
           <div className='flex justify-between'>
              <p>Shipping Fee</p>
-             <p>{currency}{delivery_fee}.00</p>
+             <p>{currency}{shippingFee}.00</p>
           </div>
           <hr />
           <div className='flex justify-between'>
               <p>Total</p>
-              <p>{currency}{getCartAmount() === 0 ? 0 : getCartAmount() + delivery_fee}.00</p>
+              <p>{currency}{totalAmount}.00</p>
           </div>
       </div>
     </div>
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -3,6 +3,8 @@ import { ShopContext } from '../context/ShopContext';
 import Title from '../components/Title';
 import { assets } from '../assets/frontend_assets/assets';
 import CartTotal from '../components/CartTotal';
+import { toast } from 'react-toastify';
+
 
 function Cart() {
   const { products, currency, cartItems, updateQuantity,navigate } = useContext(ShopContext);
@@ -78,7 +80,14 @@ function Cart() {
           <div className='w-full sm:w-[450px]'>
                 <CartTotal/>
                 <div className=' w-full text-end'>
-                     <button onClick={() => navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO PAY</button>
+                     <button onClick={() => {
+                       if(cartData.length === 0){
+                        toast.error("Please add Items in the cart")
+                       }
+                       else{
+                        navigate('/place-order')
+                       }
+                     }} className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO PAY</button>
                 </div>
           </div>
       </div>
